fd23d03c49b571ac70ee6c0c748c856703c99157
DevKaioD <kaiodavidson@gmail.com>
2024-06-06 18:31:21 -0300

    js att

--- login-page.js
+++ login-page.js
@@ -1,3 +1,11 @@
 const signUp_btn = document.querySelector('#signUp-btn');
 const signIn_btn = document.querySelector('#signIn-btn');
 const container = document.querySelector('.container');
+
+signUp_btn.addEventListener('click', () => {
+    container.classList.add("signUp-mode");
+});
+
+signIn_btn.addEventListener('click', () => {
+    container.classList.remove("signUp-mode");
+});
